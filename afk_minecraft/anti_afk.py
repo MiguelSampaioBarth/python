12a9bee0599bae1822f38d3b15b433dcd3658189
MiguelSampaioBarth <123519490+MiguelSampaioBarth@users.noreply.github.com>
2024-01-17 14:51:38 -0300

    pequenas mudanças sobre timing e variaveis

--- afk_minecraft/anti_afk.py
+++ afk_minecraft/anti_afk.py
@@ -7,13 +7,17 @@ escolha = input('escolha a opção:\n1 = FOLHAS\nDigite qualquer outra coisa par
 
 time.sleep(3) #tempo de espera.
 
-# ADICIONAR : PULOS ALEATORIOS, MECHER TELA E CLICK ALEATORIO----------------------------------------------------
+# ADICIONAR : CLICK ALEATORIO
 
-# numero QUASE aleatório
+# numeros QUASE aleatório
 num1 = random.randint(1,69)
 num2 = random.randint(1,89)
-x = random.randint(1, 110)
-tempo_aleatorio = random.randint(280,340) 
+
+x = random.randint(1, 2)  
+y = random.randint(1, 2)
+tempo_aleatorio = random.randint(280,340) # tempo parado
+cliques_aleatorios = random.randint(1,5) # numero de cliques 
+
 
 numero1 = (1 + (num1/100) + (num2/100) + (num2/100))/(1 + (num1/100))
 numero2 = numero1/3
@@ -24,6 +28,7 @@ numero6 = numero1/12
 numero7 = numero1/13
 
 def calmo () :
+   
     with pyautogui.hold('shift'): # Para frente
         time.sleep(numero4)
         pyautogui.keyDown('w')
@@ -32,13 +37,13 @@ def calmo () :
             pyautogui.keyUp('shift')
             time.sleep(numero2)
             if x >= 70 :
-                pyautogui.click(clicks=2, interval=numero3)
+                pyautogui.click(clicks=1)
             pyautogui.keyDown('shift')
             time.sleep(numero7)    
         pyautogui.keyUp('w')
         time.sleep(numero2)
         if x >= 70 :
-            pyautogui.click(clicks=2, interval=numero3)
+            pyautogui.click(clicks=1)
         pyautogui.keyUp('shift')
         time.sleep(numero3)
 
@@ -49,43 +54,47 @@ def calmo () :
             pyautogui.keyUp('shift')
             time.sleep(numero2)
             pyautogui.keyDown('shift')
-            time.sleep(numero7)    
+            time.sleep(numero7)   
+
         pyautogui.keyUp('s')
         time.sleep(numero5)
         pyautogui.press('shift')
         time.sleep(numero3)
 
 def apressado () :
-    time.sleep(numero4)
+            
     pyautogui.keyDown('w') # Para frente
-    if x >= 80 :
+    time.sleep(numero5)
+    pyautogui.keyUp('w')
+    if x == y :
         pyautogui.keyDown('shift')
         time.sleep(numero7)    
         pyautogui.keyUp('shift')
-    time.sleep(numero5)
-    pyautogui.keyUp('w')
+
     time.sleep(numero2)
 
     time.sleep(numero3)
 
-    if x >= 70 :
-        pyautogui.click(clicks=2, interval=numero3)
+    if x == y :
+        pyautogui.click(clicks=1)
+
     time.sleep(numero2)
     pyautogui.keyDown('s') # Para trás
     time.sleep(numero7)
     pyautogui.keyDown('shift')
     time.sleep(numero5)
 
-    if x >= 70 :
-        pyautogui.click(clicks=2, interval=numero3)
-    time.sleep(numero1)
+    if x == y :
+        pyautogui.click(clicks=1)
+
+    time.sleep(numero4)
     pyautogui.keyUp('s')
-    time.sleep(numero7)
+    time.sleep(numero5)
     pyautogui.keyUp('shift')
     time.sleep(numero3)
 
 while 1 :
-    time.sleep(tempo_aleatorio)
+    time.sleep(4)
         
     if escolha == '1' :
         apressado()
