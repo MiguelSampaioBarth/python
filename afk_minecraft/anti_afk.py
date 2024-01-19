017c52baae85c67221995fecd563a1812f99a178
MiguelSampaioBarth <123519490+MiguelSampaioBarth@users.noreply.github.com>
2024-01-19 12:45:29 -0300

    Aquivo otimizado e funcional de AFK para servidores de minecraft.

--- afk_minecraft/anti_afk.py
+++ afk_minecraft/anti_afk.py
@@ -3,75 +3,71 @@ import pyautogui
 import time
 import random
 
-escolha = input('escolha a opção:\n1 = FOLHAS\nDigite qualquer outra coisa para CAVERNA.')
+escolha = input('escolha a opção:\n1 = FOLHAS\nDigite qualquer outra coisa para CAVERNA.\n')
 
 time.sleep(3) #tempo de espera para iniciar a ação.
 
-def calmo () :
+def shift():
+    if x >= 70 :
+        pyautogui.keyUp('shift')
+        time.sleep(numero2)
+        if x >= 70 :
+            pyautogui.click(clicks=1)
+        else:
+            None
+        pyautogui.keyDown('shift')
+        time.sleep(numero7)
+    else:
+        None
+
+def clique():
+    if x == y :
+        pyautogui.click(clicks=1)
+    else:
+        None
+
+def Pedras () :
    
     with pyautogui.hold('shift'): # Para frente
         time.sleep(numero4)
         pyautogui.keyDown('w')
         time.sleep(numero5)
-        if x >= 70 :
-            pyautogui.keyUp('shift')
-            time.sleep(numero2)
-            if x >= 70 :
-                pyautogui.click(clicks=1)
-            pyautogui.keyDown('shift')
-            time.sleep(numero7)    
+        shift()  
+
         pyautogui.keyUp('w')
         time.sleep(numero2)
-        if x >= 70 :
-            pyautogui.click(clicks=1)
-        pyautogui.keyUp('shift')
+        shift()
+        
         time.sleep(numero3)
 
     with pyautogui.hold('shift'): # Para Trás
         time.sleep(numero3)
         pyautogui.keyDown('s')
-        if x >= 70 :
-            pyautogui.keyUp('shift')
-            time.sleep(numero2)
-            pyautogui.keyDown('shift')
-            time.sleep(numero7)   
+        shift()
 
         pyautogui.keyUp('s')
         time.sleep(numero5)
         pyautogui.press('shift')
         time.sleep(numero3)
 
-def apressado () :
+def Folhas () :
             
     pyautogui.keyDown('w') # Para frente
     time.sleep(numero5)
     pyautogui.keyUp('w')
-    if x == y :
-        pyautogui.keyDown('shift')
-        time.sleep(numero7)    
-        pyautogui.keyUp('shift')
-
-    time.sleep(numero2)
-
-    time.sleep(numero3)
-
-    if x == y :
-        pyautogui.click(clicks=1)
+    shift()
 
     time.sleep(numero2)
     pyautogui.keyDown('s') # Para trás
     time.sleep(numero7)
     pyautogui.keyDown('shift')
-    time.sleep(numero5)
-
-    if x == y :
-        pyautogui.click(clicks=1)
-    
-    time.sleep(numero4)
-    pyautogui.keyUp('s')
-    time.sleep(numero5)
-    pyautogui.keyUp('shift')
     time.sleep(numero3)
+    clique()
+    
+    pyautogui.keyUp('s')
+    time.sleep(numero3)
+    pyautogui.keyUp('shift')
+    time.sleep(numero6)
 
 while 1 :
 
@@ -80,30 +76,30 @@ while 1 :
 
     numero1 = (1 + (num1/100) + (num2/100) + (num2/100))/(1 + (num1/100))
     numero2 = numero1/3
-    numero3 = numero1/4
-    numero4 = numero1/5
-    numero5 = numero1/6
+    numero3 = numero1/5
+    numero4 = numero1/6
+    numero5 = numero1/7
     numero6 = numero1/12
     numero7 = numero1/13
 
-    tempo = random.randint(240,330) # tempo parado --------------------IMPORTANTE-------------------
+    tempo = random.randint(280,330) # tempo parado --------------------IMPORTANTE-------------------
     tempo_aleatorio = tempo + numero1
 
     cliques_aleatorios = random.randint(1,5) # numero de cliques
 
-    x = random.randint(1, 4)  
-    y = random.randint(1, 5)    
+    x = random.randint(1, 10)  
+    y = random.randint(1, 10)    
 
     time.sleep(tempo_aleatorio)
         
     if escolha == '1' :
-        apressado()
+        Folhas()
         continue
     else :
         if x >= 55 :
-            apressado()
+            Folhas()
             continue
         else :
-            calmo()
+            Pedras()
             continue
         
